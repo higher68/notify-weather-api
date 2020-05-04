c24a8f06eba6059863d4ff22722a477f00431fb7
junya <hatiro7code@gmail.com>
2020-05-04 22:53:49 +0900

    refs #88 Configをsingletonに設定

--- api/src/__test__/environment.test.ts
+++ api/src/__test__/environment.test.ts
@@ -1,16 +1,15 @@
 import { Config } from '../config'
 
 describe('refer environment variable', () => {
+    const env = process.env;
+    env.X_RAPIDAPI_HOST = 'test1';
+    env.X_RAPIDAPI_KEY = 'test2';
     test('radidapi host', () => {
-        const env = process.env;
-        env.X_RAPIDAPI_HOST = 'test1';
-        const config = new Config();
+        const config = Config.getInstance();
         expect(config.x_rapidapi_host).toBe('test1');
     });
     test('rapidapi key', () => {
-        const env = process.env;
-        env.X_RAPIDAPI_KEY = 'test2';
-        const config = new Config();
+        const config = Config.getInstance();
         expect(config.x_rapidapi_key).toBe('test2');
     });
 });
--- api/src/config.ts
+++ api/src/config.ts
@@ -1,18 +1,26 @@
 export class Config {
+    private static config: Config;
     private readonly X_RAPIDAPI_HOST: string;
     private readonly X_RAPIDAPI_KEY: string;
 
-    constructor() {
+    private constructor() {
         const env = process.env;
         this.X_RAPIDAPI_HOST = env.X_RAPIDAPI_HOST;
         this.X_RAPIDAPI_KEY = env.X_RAPIDAPI_KEY;
     }
 
-    get x_rapidapi_host() {
+    public static getInstance() {
+        if (this.config == null) {
+            this.config = new Config();
+        }
+        return this.config;
+    }
+
+    public get x_rapidapi_host() {
         return this.X_RAPIDAPI_HOST;
     }
 
-    get x_rapidapi_key() {
+    public get x_rapidapi_key() {
         return this.X_RAPIDAPI_KEY;
     }
 }
